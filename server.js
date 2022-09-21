e4b73b01b06b25d5bf073af36d5f626fac0f9572
Ekaterina Larionova <ekaterinalarionova25@gmail.com>
2022-09-21 15:36:31 +0300

    фикс комментариев

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+process.env.PORT = 3000;
 const path = require('path');
 
 const dirPath = path.join(__dirname, './');
