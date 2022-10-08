dda0e4daa2b7b38dac7ef878e29f1256c6d1e5cd
Ekaterina Larionova <ekaterinalarionova25@gmail.com>
2022-10-09 00:40:33 +0300

    добавлены регулярки

--- src/components/avatar-input/avatar-input.ts
+++ src/components/avatar-input/avatar-input.ts
@@ -1,5 +1,4 @@
 import Block from '../../core/Block';
-import DefaultAvatar from "../../noimage.png";
 
 import './avatar-input.css';
 
@@ -12,7 +11,7 @@ export class AvatarInput extends Block {
             <label class="label">
             <input type="file"/>
             <figure class="personal-figure">
-             <img src="${DefaultAvatar}" class="personal-avatar" alt="avatar">
+             <img src="https://raw.githubusercontent.com/ThiagoLuizNunes/angular-boilerplate/master/src/assets/imgs/camera-white.png" class="personal-avatar" alt="avatar">
             <figcaption class="personal-figcaption">
             <img src="https://raw.githubusercontent.com/ThiagoLuizNunes/angular-boilerplate/master/src/assets/imgs/camera-white.png">
             </figcaption>
